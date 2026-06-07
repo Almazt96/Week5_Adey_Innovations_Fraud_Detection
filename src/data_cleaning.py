e86cc03e0e56a1f45e5636fd0286e22ce0e88d33
Almaz <Almazt96@gmail.com>
2026-06-07 13:39:45 +0300

    Revised as per rubric feedback

--- src/data_cleaning.py
+++
@@ -1,3 +0,0 @@
-# Automated string-to-datetime casting and duplicate cleansing for both datasets. 
-# This ensures that the data is in the correct format for analysis and modeling, 
-# and that we are not training on duplicate records which could bias our results.
